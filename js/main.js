bfff8e11e56e1bd323fb5342000df9a93883dda3
Kong Yang <kmongy05@gmail.com>
2019-04-16 08:17:44 -0500

    Moved the function out to separate concerns.

--- js/main.js
+++ js/main.js
@@ -4,14 +4,18 @@ window.onload = function (){
 
   http.onreadystatechange = function() {
     if (http.readyState == 4 && http.status == 200) {
-      let dashboardView = document.getElementById("dashboard-view-cont");
-      let addStudent = document.getElementById("addStudent").addEventListener("click", revealData);
-
-      function revealData() {
-        dashboardView.innerHTML = this.responseText;
-      }
+      viewingInfo(this);
     }
   };
   http.open("GET", "dataAdd.html", true);
   http.send();
+}
+
+function viewingInfo() {
+  let dashboardView = document.getElementById("dashboard-view-cont");
+  let addStudent = document.getElementById("addStudent").addEventListener("click", revealData);
+
+  function revealData() {
+    dashboardView.innerHTML = this.responseText;
+  }
 }
