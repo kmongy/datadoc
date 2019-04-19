6ee554dd7c490c3c29e067ee264795ff7f674251
Kong Yang <kmongy05@gmail.com>
2019-04-19 08:50:42 -0500

    Moving prototype forward. Will work on JS at a later time. Transitioning to just links in html.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-window.onload = function (){
+// window.onload = function (){
 
-  let http = new XMLHttpRequest();
+//   let http = new XMLHttpRequest();
 
-  http.onreadystatechange = function() {
-    if (http.readyState == 4 && http.status == 200) {
-      viewingInfo(this);
-    }
-  };
-  http.open("GET", "dataAdd.html", true);
-  http.send();
-}
+//   http.onreadystatechange = function() {
+//     if (http.readyState == 4 && http.status == 200) {
+//       viewingInfo(this);
+//     }
+//   };
+//   http.open("GET", "dataAdd.html", true);
+//   http.send();
+// }
 
-function viewingInfo() {
-  let dashboardView = document.getElementById("dashboard-view-cont");
-  let addStudent = document.getElementById("addStudent").addEventListener("click", revealData);
+// function viewingInfo() {
+//   let dashboardView = document.getElementById("dashboard-view-cont");
+//   let addStudent = document.getElementById("addStudent").addEventListener("click", revealData);
 
-  function revealData() {
-    dashboardView.innerHTML = this.responseText;
-  }
-}
+//   function revealData() {
+//     dashboardView.innerHTML = this.responseText;
+//   }
+// }
