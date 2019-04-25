1096ef77132ad270a72856223e50d7bc55c469f0
Kong Yang <kmongy05@gmail.com>
2019-04-25 12:14:32 -0500

    Back to regular links for time constraints on this prototype.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-function addStudent() {
-  let addingStudentHtml = new XMLHttpRequest();
-  addingStudentHtml.onload = function() {
-    if (addingStudentHtml.readyState == 4 && addingStudentHtml.status == 200) {
-      document.getElementById("dashboard-view-cont").innerHTML = this.responseText;
-    }
-  };
+// function addStudent() {
+//   let addingStudentHtml = new XMLHttpRequest();
+//   addingStudentHtml.onload = function() {
+//     if (addingStudentHtml.readyState == 4 && addingStudentHtml.status == 200) {
+//       document.getElementById("dashboard-view-cont").innerHTML = this.responseText;
+//     }
+//   };
 
-  addingStudentHtml.open("GET", "dataAdd.html", true);
-  addingStudentHtml.send();
-}
+//   addingStudentHtml.open("GET", "dataAdd.html", true);
+//   addingStudentHtml.send();
+// }
